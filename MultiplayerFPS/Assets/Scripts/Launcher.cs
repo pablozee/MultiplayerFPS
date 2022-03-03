ee7623cb2bcc98014d0683302bd6ac3a8de23f36
Zeshan Rasul <zeshanrasul@gmail.com>
2022-03-03 22:05:12 +0000

    Room creation and ui

--- MultiplayerFPS/Assets/Scripts/Launcher.cs
+++ MultiplayerFPS/Assets/Scripts/Launcher.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using TMPro;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
     [SerializeField] private string titleMenuName = "Title";
     [SerializeField] private string loadingMenuName = "Loading";
+    [SerializeField] private string roomMenuName = "Room";
+    [SerializeField] private string errorMenuName = "Error";
+    [SerializeField] private TMP_InputField roomNameInputField;
+    [SerializeField] private TMP_Text roomNameText;
+    [SerializeField] private TMP_Text errorText;
 
     void Start()
     {
@@ -28,8 +34,37 @@ public class Launcher : MonoBehaviourPunCallbacks
         Debug.Log("Joined Lobby");
     }
 
-    void Update()
+    public void CreateRoom()
     {
-        
+        if (string.IsNullOrEmpty(roomNameInputField.text)) return;
+
+        PhotonNetwork.CreateRoom(roomNameInputField.text);
+        Debug.Log("Creating room " + roomNameInputField.text);
+        MenuManager.Instance.OpenMenu(loadingMenuName);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined room " + PhotonNetwork.CurrentRoom.Name);
+        roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+        MenuManager.Instance.OpenMenu(roomMenuName);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        errorText.text = "Room Creation Failed " + message;
+        MenuManager.Instance.OpenMenu(errorMenuName);
+    }
+
+    public void LeaveRoom()
+    {
+        Debug.Log("Leaving room " + PhotonNetwork.CurrentRoom.Name);
+        PhotonNetwork.LeaveRoom();
+        MenuManager.Instance.OpenMenu(loadingMenuName);
+    }
+
+    public override void OnLeftRoom()
+    {
+        MenuManager.Instance.OpenMenu(titleMenuName);
     }
 }
